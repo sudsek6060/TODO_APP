826d970da1e2982ac7cdf9e3630fe7386e601bd3
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-03 17:25:15 +0530

    delet functionality

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const input = document.querySelector('.input');
 const addBtn = document.querySelector('.addButton');
 const container = document.querySelector('.container')
-const deletBtn = document.querySelector('.deleteButton')
-// console.log(input);
+
+
 
 const markup = function (){
     return  `
@@ -10,18 +10,29 @@ const markup = function (){
     <div class="item">
             <h3 class="item_input">${input.value}</h3>
             <button class="editButton">Edit</button>
-            <button class="deleteButton">Delete</button>
+            <button class="deleteButton" >Delete</button>
         </div>
 `
 }
-const addText = function(){
-   
-    const html = markup()
-    
-   return container.insertAdjacentHTML('afterbegin', html)
-}
+
+
 
 addBtn.addEventListener('click', function(e){
     e.preventDefault();
-    addText()
+    
+   
+    const html = markup()
+    
+    container.insertAdjacentHTML('afterbegin', html);
+
+    var current_tasks = document.querySelectorAll(".deleteButton");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
 })
+
+
+// deletBtn.addEventListener('click', deleteText())
